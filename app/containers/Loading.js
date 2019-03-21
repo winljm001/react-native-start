c309f6b868f084133b47eed25cc04d6577affee9
李佳明 <lijiaming@iwubida.com>
2019-03-21 10:48:40 +0800

    test4

--- app/containers/Loading.js
+++ app/containers/Loading.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import React from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 
 const Loading = () => (
   <View style={styles.container}>
     <ActivityIndicator />
   </View>
-)
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
-export default Loading
+export default Loading;
